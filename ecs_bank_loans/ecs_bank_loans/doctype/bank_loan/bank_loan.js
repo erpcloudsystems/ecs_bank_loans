81b5f048ce977765a58b41c3cb45aa266c031ec8
mghoneem10 <mghoneem10@gmail.com>
2021-09-05 00:56:33 +0000

    x

--- ecs_bank_loans/ecs_bank_loans/doctype/bank_loan/bank_loan.js
+++ ecs_bank_loans/ecs_bank_loans/doctype/bank_loan/bank_loan.js
@@ -1,12 +1,18 @@
 // Copyright (c) 2021, erpcloud.systems and contributors
 // For license information, please see license.txt
+cur_frm.add_fetch('bank_account','account','current_account');
+cur_frm.add_fetch('bank_account','collection_fee_account','receivable_account');
+cur_frm.add_fetch('bank_account','payable_account','payable_account');
+cur_frm.add_fetch('loan_type','rate_of_interest','rate_of_interest');
 
 frappe.ui.form.on("Bank Loan", {
 	setup: function(frm) {
 		frm.set_query("bank_account", function() {
 			return {
 				filters: [
-					["Bank Account","bank", "in", frm.doc.applicant]
+					["Bank Account","bank", "in", frm.doc.applicant],
+					["Bank Account","is_company_account", "=", 1],
+
 				]
 			};
 		});
@@ -57,32 +63,6 @@ frappe.ui.form.on("Bank Loan", "validate", function(frm) {
     }
 });
 
-frappe.ui.form.on("Bank Loan", {
-    validate:function(frm, cdt, cdn){
-        var dw = locals[cdt][cdn];
-        var total1 = 0;
-        var total2 = 0;
-        var total3 = 0;
-        frm.doc.repayment_schedule.forEach(function(dw) { total1 += dw.principal_amount; });
-        frm.doc.repayment_schedule.forEach(function(dw) { total2 += dw.interest_amount; });
-        frm.doc.repayment_schedule.forEach(function(dw) { total3 += dw.total_payment; });
-        frm.set_value("total_principal_payable", total1);
-        frm.set_value("total_interest_payable", total2);
-        frm.set_value("total_payment", total3);
-        refresh_field("total_principal_payable");
-        refresh_field("total_interest_payable");
-        refresh_field("total_payment");
-    }
-});
-
-//frappe.ui.form.on("Bank Loan","validate", function(frm){
-//    var x = frm.doc.no_of_repayments;
-//    cur_frm.doc.repayment_schedule.length = x;
-//    for (var i =0; i < x; i++){
-//    cur_frm.doc.repayment_schedule[i].total_payment = cur_frm.doc.repayment_amount;
-//    }
-//    cur_frm.refresh_field('repayment_schedule');
-//});
 
 //frappe.ui.form.on("Bank Loan","validate", function(){
 //    if (cur_frm.doc.repayment_schedule.length != cur_frm.doc.no_of_repayments){
--- ecs_bank_loans/ecs_bank_loans/doctype/bank_loan/bank_loan.py
+++ ecs_bank_loans/ecs_bank_loans/doctype/bank_loan/bank_loan.py
@@ -9,6 +9,11 @@ from frappe.desk.search import sanitize_searchfield
 from frappe.utils import (flt, getdate, get_url, now,
 nowtime, get_time, today, get_datetime, add_days)
 from frappe.utils import add_to_date, now, nowdate
+import frappe, math, json
+import erpnext
+from frappe import _
+from six import string_types
+from frappe.utils import flt, rounded, add_months, nowdate, getdate, now_datetime
 
 class BankLoan(Document):
 	@frappe.whitelist()
@@ -17,7 +22,9 @@ class BankLoan(Document):
 
 	@frappe.whitelist()
 	def validate(self):
-		self.set_repayments()
+		#self.set_repayments()
+		self.calculate_totals()
+		self.calculate_repayment_amount()
 
 	@frappe.whitelist()
 	def show_warnings(self):
@@ -30,18 +37,46 @@ class BankLoan(Document):
 		if not self.payable_account:
 			frappe.throw(_(" برجاء تحديد حساب برسم الدفع داخل الحساب البنكي وإعادة إختيار الحساب البنكي مرة أخرى "))
 
-	'''@frappe.whitelist()
-	def set_repayments(self):
-		docs = frappe.db.sql(""" select repayment_start_date, repayment_every, repayment_amount, payment_frequency from `tabBank Loan` where `tabBank Loan`.name = %s """, self.name, as_dict=1)
-		x = 0
-		y = self.no_of_repayments
-		for x in self.repayment_schedule and x <= y:
-			z = self.append("repayment_schedule", {})
-			z.payment_date = self.repayment_start_date
-			z.total_payment = self.repayment_amount
-			x += 1
-	'''
+	def calculate_repayment_amount(self):
+		if self.calculate_repayment_schedule_automatically:
+			import numpy as np
+			self.repayment_amount = -1 * np.pmt(0.01 * self.rate_of_interest / (360 / self.repayment_every), self.no_of_repayments, self.net_loan_amount)
+			self.repayment_schedule = []
+			payment_date = self.repayment_start_date
+			balance_amount = self.net_loan_amount
+			while (balance_amount > 0):
+				interest_amount = self.interest_amount
+				principal_amount = self.repayment_amount - interest_amount
+				balance_amount = flt(balance_amount + interest_amount - self.repayment_amount)
+				if balance_amount < 0:
+					principal_amount += balance_amount
+					balance_amount = 0.0
 
+				total_payment = principal_amount + interest_amount
+				self.append("repayment_schedule", {
+					"payment_date": payment_date,
+					"principal_amount": principal_amount,
+					"interest_amount": interest_amount,
+					"total_payment": total_payment,
+					"balance_loan_amount": balance_amount
+				})
+				next_payment_date = add_days(payment_date, self.repayment_every)
+				payment_date = next_payment_date
+
+	def calculate_totals(self):
+		self.total_payment = 0
+		self.total_interest_payable = 0
+		self.total_principal_payable = 0
+
+		for data in self.repayment_schedule:
+			self.total_payment += data.total_payment
+			self.total_interest_payable +=data.interest_amount
+			self.total_principal_payable += data.principal_amount
+
+	pass
+
+
+'''
 	def set_repayments(self):
 		if not self.repayment_start_date:
 			frappe.throw(_("Repayment Start Date is mandatory"))
@@ -68,8 +103,7 @@ class BankLoan(Document):
 				})
 				next_payment_date = add_days(payment_date, self.repayment_every)
 				payment_date = next_payment_date
-
-	pass
+'''
 
 
 
